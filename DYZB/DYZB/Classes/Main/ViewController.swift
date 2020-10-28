ed297da5b3c25c9fdd51c6e6fb4bb0e239365f74
saber <wwjwblc@163.com>
2020-10-28 10:43:41 +0800

    配置

---
+++ DYZB/DYZB/Classes/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by saber on 2020/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
