b89fe153b11faf9d5ae87be5286383c805967b02
saber <wwjwblc@163.com>
2020-10-31 14:11:24 +0800

    pod

---
+++ DYZB/DYZB/Classes/Tools/NetworkTools.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkTools.swift
+//  DYZB
+//
+//  Created by saber on 2020/10/31.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+enum MethodType {
+    case get
+    case post
+    case dele
+}
+struct NetworkTools {
+    
+}
+
+//struct NetworkTools {
+//
+//}
